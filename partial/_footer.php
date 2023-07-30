2380eadfa615281a6c35785e0c9e3fa8651d77dd
candagdelen0 <candagdelen0@gmail.com>
2023-07-30 15:00:32 +0300

    Blog | footer oluşturuldu

---
+++ partial/_footer.php
@@ -0,0 +1,8 @@
+<footer>
+    <div class="text-center p-3 fixed-bottom" style="background-color: rgba(0, 0, 0, 0.2);">
+        © 2023 Copyright:
+            <a class="text-dark" style="text-decoration: none;">Can Dağdelen</a> Tüm Hakları Saklıdır.
+    </div>
+</footer>
+</body>
+</html>
