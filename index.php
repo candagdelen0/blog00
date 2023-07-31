65cd08288a63f3f349cebae5afbda37d71cf8dba
candagdelen0 <candagdelen0@gmail.com>
2023-07-31 13:28:46 +0300

    Blog | Anasayfa içeriklerini tamamlama

--- index.php
+++ index.php
@@ -1,5 +1,16 @@
 <?php
 include("partial/_header.php");
 include("partial/_navbar.php");
+?>
+<div class="container">
+    <div class="row">
+        <div class="col-md-8">
+            <?php include ("partial/_content.php"); ?>  
+        </div>
+        <div class="col-md-4">
+            <?php include ("partial/_about.php"); ?>
+        </div>
+    </div>
+</div>
 
-include("partial/_footer.php");
+<?php include("partial/_footer.php"); ?>
