b98d83f979feea7251eeed4cdc2666c6274fd636
candagdelen0 <candagdelen0@gmail.com>
2023-07-31 13:30:19 +0300

    Blog | Hakkımızda İçeriği

---
+++ partial/_about.php
@@ -0,0 +1,10 @@
+
+<div class="card mt-3">
+    <div class="row text-center">
+        <div class="col-md-12 mt-2"><img src="" class="rounded-circle"></div>
+        <div class="col-md-12">
+            <h5 class="card-title mt-2">Hakkımda</h5>
+            <p class="card-text me-2 ms-2">Lorem ipsum dolor sit amet.</p>
+        </div>
+    </div>
+</div>
