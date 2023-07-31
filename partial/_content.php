a6dcbdea96a5a2102cdc6c8d3ce3b45961eb4dac
candagdelen0 <candagdelen0@gmail.com>
2023-07-31 13:29:05 +0300

    Blog | içerik Sayfası

---
+++ partial/_content.php
@@ -0,0 +1,10 @@
+
+<div class="card mb-3 mt-3">
+    <div class="row">
+        <div class="col-md-4 text-center mt-3">Resim</div>
+        <div class="col-md-8 p-2">
+            <h5 class="card-title">Şehir / <em>Ülke</em></h5>
+            <p class="card-text me-2">Yazı</p>
+        </div>
+    </div>
+</div>
