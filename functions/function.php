0f622d54d1af508d876cd0c13879f64eb7d05b96
candagdelen0 <candagdelen0@gmail.com>
2023-08-01 16:47:39 +0300

    Anasayfa | şehir getirme Fonksiyonu

--- functions/function.php
+++ functions/function.php
@@ -9,10 +9,18 @@ function safety($data) {
 }
 
 function getAbout($db) {
-    $query = $db->prepare("SELECT * FROM about", PDO::FETCH_ASSOC);
+    $query = $db->prepare("SELECT * FROM about", PDO::FETCH_ASSOC)->fetch();
     if ($query != false && !empty($query)) {
         return $query;
     }
 }
+
+function getCities($db) {
+    $query = $db->prepare("SELECT * FROM sehirler LIMIT 3", PDO::FETCH_ASSOC)->fetchAll();
+    if ($query != false && !empty($query)) {
+        return $query;
+    }
+}
+
 
 ?>
