0179543b9e59f512195498e3cb00dadd20620505
candagdelen0 <candagdelen0@gmail.com>
2023-08-01 16:52:09 +0300

    Navbar | Link atanması

--- partial/_navbar.php
+++ partial/_navbar.php
@@ -2,9 +2,9 @@
     <div class="container">
         <a href="index.php" class="navbar-brand"><b>Seyahatname</b></a>
         <ul class="navbar-nav me-auto">
-            <li class="nav-item"><a href="#" class="nav-link">Türkiye</a></li>
-            <li class="nav-item"><a href="#" class="nav-link">Yurtdışı</a></li> 
-            <li class="nav-item"><a href="#" class="nav-link">Seyahat Önerileri</a></li> 
+            <li class="nav-item"><a href="yurtici.php" class="nav-link">Türkiye</a></li>
+            <li class="nav-item"><a href="yurtdisi.php" class="nav-link">Yurtdışı</a></li> 
+            <li class="nav-item"><a href="oneriler.php" class="nav-link">Seyahat Önerileri</a></li> 
         </ul>
         <ul class="navbar-nav me-2">
             <li class="nav-item"><a href="login.php" class="nav-link">Login</a></li>
