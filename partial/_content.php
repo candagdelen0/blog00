61300aa16aa32b1423d5eaadee4e14f8d07a52a6
candagdelen0 <candagdelen0@gmail.com>
2023-08-01 16:50:25 +0300

    Anasayfa | fonksiyonun entegresi

--- partial/_content.php
+++ partial/_content.php
@@ -1,10 +1,18 @@
+<?php
+require "functions/connection.php";
+require "functions/function.php";
 
-<div class="card mb-3 mt-3">
+$cities = getCities($db);
+foreach ($cities as $city) {
+    echo '<div class="card mb-3 mt-3">
     <div class="row">
-        <div class="col-md-4 text-center mt-3">Resim</div>
+        <div class="col-md-4 text-center mt-3"><img src=" '.$city["sehirResmi"].' "></div>
         <div class="col-md-8 p-2">
-            <h5 class="card-title">Şehir / <em>Ülke</em></h5>
-            <p class="card-text me-2">Yazı</p>
+            <h5 class="card-title"><a href="#" style="text-decoration: none;" class="text-dark">'.$city["sehirAdi"].' / <em>'.$city["ulkeAdi"].'</em></a></h5>
+            <p class="card-text me-2">'.$city["sehirAciklama"].'</p>
         </div>
     </div>
-</div>
+</div>';
+}
+
+
