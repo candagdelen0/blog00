5564427959d26741ad9df690a78217d9262aff9b
candagdelen0 <candagdelen0@gmail.com>
2023-08-01 16:43:11 +0300

    Hakkımızda | Sayfa içine db entegre

--- partial/_about.php
+++ partial/_about.php
@@ -1,10 +1,10 @@
-
+<?php @$about = getAbout($db); ?>
 <div class="card mt-3">
     <div class="row text-center">
-        <div class="col-md-12 mt-2"><img src="" class="rounded-circle"></div>
+        <div class="col-md-12 mt-2"><img src="<?php echo $about["picture"]; ?>" class="rounded-circle"></div>
         <div class="col-md-12">
-            <h5 class="card-title mt-2">Hakkımda</h5>
-            <p class="card-text me-2 ms-2">Lorem ipsum dolor sit amet.</p>
+            <h5 class="card-title mt-2"><?php echo $about["title"]; ?></h5>
+            <p class="card-text me-2 ms-2"><?php echo $about["aboutText"]; ?></p>
         </div>
     </div>
 </div>
