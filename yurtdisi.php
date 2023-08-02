c70af9325e69ff80d22961cb8c5d740a411f58af
candagdelen0 <candagdelen0@gmail.com>
2023-08-02 15:23:09 +0300

    Link Sayfası 2 | Anasayfa

---
+++ yurtdisi.php
@@ -0,0 +1,14 @@
+<?php
+require "functions/connection.php";
+include ("partial/_header.php");
+include ("partial/_navbar.php");
+?>
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 mx-auto">
+            <?php include ("partial/_yurtdisicerik.php"); ?>
+        </div>
+    </div>
+</div>
+
+<?php include ("partial/_footer.php"); ?>
