677e4227172a17ae634561a2c0d959840ea3c91a
candagdelen0 <candagdelen0@gmail.com>
2023-08-06 18:31:48 +0300

    Hakkımızda | class'tan fonksiyonu çağırma

--- partial/_about.php
+++ partial/_about.php
@@ -1,10 +1,16 @@
-<?php @$about = getAbout($db); ?>
-<div class="card mt-3">
-    <div class="row text-center">
-        <div class="col-md-12 mt-2"><img src="<?php echo $about["picture"]; ?>" class="rounded-circle"></div>
-        <div class="col-md-12">
-            <h5 class="card-title mt-2"><?php echo $about["title"]; ?></h5>
-            <p class="card-text me-2 ms-2"><?php echo $about["aboutText"]; ?></p>
+<?php 
+    require_once "functions/connection.php"; 
+    require_once "functions/function.php"; 
+    
+    $about = new Blog();
+    foreach ($about->getAbout() as $item):
+        ?><div class="card mt-3">
+        <div class="row text-center">
+            <div class="col-md-12 mt-2"><img src="<?php echo $item->picture; ?>" class="rounded-circle"></div>
+            <div class="col-md-12">
+                <h5 class="card-title mt-2"><?php echo $item->title; ?></h5>
+                <p class="card-text me-2 ms-2"><?php echo $item->aboutText; ?></p>
+            </div>
         </div>
     </div>
-</div>
+    <?php endforeach; ?>
