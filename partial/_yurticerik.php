47bb2d561ba4e4ddfafb5dd9b7a3c68619dd0e22
candagdelen0 <candagdelen0@gmail.com>
2023-08-06 18:50:36 +0300

    Link Sayfası 1

--- partial/_yurticerik.php
+++ partial/_yurticerik.php
@@ -1,19 +1,20 @@
 <?php
-require "functions/connection.php";
 require "functions/function.php";
 
-$yurtici = getSpecialCities($db);
-foreach ($yurtici as $tr) {
-    if ($tr["ulkeAdi"] == "Türkiye") {
-        echo '<div class="card mb-3 mt-3">
+$sistem = new Blog();
+$sql = "SELECT * FROM sehirler";
+foreach ($sistem->getCities($sql) as $item):
+    if ($item->ulkeAdi == "Türkiye"):
+        ?><div class="card mb-3 mt-3">
             <div class="row">
-                <div class="col-md-4 text-center mt-3"><img src=" '.$tr["sehirResmi"].' " class="rounded"></div>
+                <div class="col-md-4 text-center mt-3"><img src="<?php echo $item->sehirResmi; ?>" class="rounded"></div>
                 <div class="col-md-8 p-2">
-                    <h5 class="card-title"><a href="#" style="text-decoration: none;" class="text-dark">'.$tr["sehirAdi"].'</a></h5>
-                    <p class="card-text me-2">'.$tr["sehirAciklama"].'</p>
+                    <h5 class="card-title"><a href="#" style="text-decoration: none;" class="text-dark"><?php echo $item->sehirAdi; ?></a></h5>
+                    <p class="card-text me-2"><?php echo $item->sehirAciklama; ?></p>
                 </div>
             </div>
-        </div>';
-    }
-}
+        </div>
+<?php 
+endif;
+endforeach; 
 ?>
