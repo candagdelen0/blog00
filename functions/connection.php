912619061fad5648b3036326db44e6d8f6572e6b
candagdelen0 <candagdelen0@gmail.com>
2023-08-06 18:29:32 +0300

    Veritabanı Bağlantısı Class tanımlaması

--- functions/connection.php
+++ functions/connection.php
@@ -1,14 +1,24 @@
 <?php
-try {
-    $host = 'localhost';
-    $dbname = 'blog';
-    $username = 'root';
-    $password = '';
-    $db = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
-    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $db->query("SET @@lc_time_names = 'tr_TR'");
-} catch (PDOException $e) {
-    die("Veritabanı bağlantısı başarısız: " . $e->getMessage());
-}
+class Db {
+    private $host = "localhost";
+    private $user = "root";
+    private $password = "";
+    private $dbName = "blog";
 
-?>
+    protected function connect() {
+        try {
+            $dsn = "mysql:host=".$this->host.";dbname=".$this->dbName;
+            $pdo = new PDO($dsn, $this->user, $this->password);
+
+            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
+
+            return $pdo;
+        } catch (PDOException $e) {
+            die("Veritabanı bağlantısı başarısız: ".$e->getMessage());
+        }
+    }
+
+
+
+}
