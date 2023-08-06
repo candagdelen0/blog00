f2f7243129aca1c682206c11c1791bbaf097ca71
candagdelen0 <candagdelen0@gmail.com>
2023-08-06 18:32:21 +0300

    İçerik | Class'tan fonksiyon çağırma

--- partial/_content.php
+++ partial/_content.php
@@ -1,18 +1,21 @@
-<?php
-require "functions/connection.php";
-require "functions/function.php";
-
-$cities = getCities($db);
-foreach ($cities as $city) {
-    echo '<div class="card mb-3 mt-3">
-    <div class="row">
-        <div class="col-md-4 text-center mt-3"><img src=" '.$city["sehirResmi"].' "></div>
-        <div class="col-md-8 p-2">
-            <h5 class="card-title"><a href="#" style="text-decoration: none;" class="text-dark">'.$city["sehirAdi"].' / <em>'.$city["ulkeAdi"].'</em></a></h5>
-            <p class="card-text me-2">'.$city["sehirAciklama"].'</p>
+<?php 
+    require_once "functions/connection.php"; 
+    require_once "functions/function.php"; 
+    
+    $sehirler = new Blog();
+    foreach ($sehirler->getCities() as $item):
+        ?><div class="card mb-3 mt-3">
+            <div class="row">
+                <div class="col-md-4 text-center mt-3"><img src="<?php echo $item->sehirResmi ?>"></div>
+                <div class="col-md-8 p-2">
+                    <h5 class="card-title"><a href="#" style="text-decoration: none;" class="text-dark"><?php echo $item->sehirAdi ?> / <em><?php echo $item->ulkeAdi ?></em></a></h5>
+                    <p class="card-text me-2"><?php echo $item->sehirAciklama ?> </p>
+                </div>
+            </div>
         </div>
-    </div>
-</div>';
-}
+    <?php endforeach; ?>
+
+
+
 
 
