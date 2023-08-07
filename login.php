4055681c1b987fe709198bdc34293a0e64ed723f
candagdelen0 <candagdelen0@gmail.com>
2023-08-07 12:41:54 +0300

    Login İşlemi

--- login.php
+++ login.php
@@ -1,45 +1,54 @@
 <?php
 session_start();
 require "functions/connection.php";
+require "functions/function.php";
 include "partial/_header.php";
 include "partial/_navbar.php";
-if(isset($_POST["login"])) {
+
+$sistem = new Blog();
+
+if(isset($_POST["login"])):
     $username = $_POST["username"];
     $password = $_POST["password"];
-
-    if($username != "" && $password != "") {
-        $query = $db->prepare("SELECT * FROM users WHERE username=? AND parola=?");
+    if($username == "" || $password == ""):
+        echo '<div class="col-md-4 mx-auto mt-2 alert alert-danger">Bilgiler Boş Bırakılamaz</div>';
+        header("refresh: 2, url= index.php");
+    else:
+        $sql = "SELECT * FROM users WHERE username=? AND parola=?";
+        $query = $sistem->loginControl($sql);
         $query->execute([$username,$password]);
-        $kontrol = $query->rowCount();
-        if ($kontrol > 0) {
+        
+        if ($query->rowCount() != 0):
             $_SESSION['Kullanici'] = $username;
             header("Location: index.php");
-        } else {
+        else:
             echo '<div class="col-md-4 mx-auto mt-2 alert alert-danger">Kullanıcı Bulunamadı</div>';
             header("refresh: 2, url= index.php");
-        }
-    }
-}else {
-    ?><div class="container my-3">
-        <div class="row">
-            <div class="col-md-4 mx-auto">
-                <div class="bg-primary mt-3 p-3 fs-3 text-center">Üye Girişi</div>
-                <div class="p-4" style="background-color: #F0F8FF;">
-                    <form action="login.php" method="post">
-                        <div class="mb-3">
-                            <label for="username">Kullanıcı Adı</label>
-                            <input type="text" name="username" class="form-control">
-                        </div>
-                        <div class="mb-3">
-                            <label for="password">Parola</label>
-                            <input type="password" name="password" class="form-control">
-                        </div>
-                        <button type="submit" class="btn btn-primary" name="login">Login</button>
-                    </form>
+        endif;
+        
+    endif;
+
+        else:
+        echo '<div class="container my-3">
+            <div class="row">
+                <div class="col-md-4 mx-auto">
+                    <div class="bg-primary mt-3 p-3 fs-3 text-center">Üye Girişi</div>
+                    <div class="p-4" style="background-color: #F0F8FF;">
+                        <form action="login.php" method="post">
+                            <div class="mb-3">
+                                <label for="username">Kullanıcı Adı</label>
+                                <input type="text" name="username" class="form-control">
+                            </div>
+                            <div class="mb-3">
+                                <label for="password">Parola</label>
+                                <input type="password" name="password" class="form-control">
+                            </div>
+                            <button type="submit" class="btn btn-primary" name="login">Login</button>
+                        </form>
+                    </div>
                 </div>
             </div>
-        </div>
-    </div><?php
-    include "partial/_footer.php";
-}
+        </div>';
+    endif;
 ?>
+
