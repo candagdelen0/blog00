70666fe32b438733a9d2b18729f915b3b2c99c49
candagdelen0 <candagdelen0@gmail.com>
2023-08-07 12:43:15 +0300

    Session tanımlama

--- partial/_navbar.php
+++ partial/_navbar.php
@@ -7,12 +7,17 @@
             <li class="nav-item"><a href="oneriler.php" class="nav-link">Seyahat Önerileri</a></li> 
         </ul>
         <ul class="navbar-nav me-2">
-            <li class="nav-item"><a href="login.php" class="nav-link">Login</a></li>
-            <li class="nav-item"><a href="register.php" class="nav-link">Register</a></li>
+            <?php if (isset($_SESSION['Kullanici'])): ?>
+                <li class="nav-item"><a href="#" class="nav-link border border-primary rounded bg-primary text-white"><?php echo $_SESSION['Kullanici']; ?></a></li>
+                <li class="nav-item"><a href="logout.php" class="nav-link">Logout</a></li>
+            <?php else: ?>
+                <li class="nav-item"><a href="login.php" class="nav-link">Login</a></li>
+                <li class="nav-item"><a href="register.php" class="nav-link">Register</a></li>
+                <form action="index.php" class="d-flex" method="get">
+                    <input type="text" name="ara" class="form-control me-2" placeholder="Arama">
+                    <button type="submit" class="btn btn-warning">Ara</button>
+                </form>
+            <?php endif; ?>
         </ul>
-        <form action="index.php" class="d-flex" method="get">
-            <input type="text" name="ara" class="form-control me-2" placeholder="Arama">
-            <button type="submit" class="btn btn-warning">Ara</button>
-        </form>
     </div>
 </nav>
