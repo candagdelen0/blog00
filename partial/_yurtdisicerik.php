672d5c3637e08b7217d6804b282b4c1abf3df9ef
candagdelen0 <candagdelen0@gmail.com>
2023-08-08 20:34:22 +0300

    Link Sayfası 1 | resim entegresi

--- partial/_yurtdisicerik.php
+++ partial/_yurtdisicerik.php
@@ -7,9 +7,9 @@ foreach ($sistem->getCities($sql) as $item):
     if ($item->ulkeAdi != "Türkiye"):
         ?><div class="card mb-3 mt-3">
             <div class="row">
-                <div class="col-md-4 text-center mt-3"><img src="<?php echo $item->sehirResmi; ?>" class="rounded"></div>
+                <div class="col-md-4 text-center mt-3 pb-3"><img src="<?php echo $item->sehirResmi; ?>" class="rounded" style="width: 200px; height: 250px;"></div>
                 <div class="col-md-8 p-2">
-                    <h5 class="card-title"><a href="#" style="text-decoration: none;" class="text-dark"><?php echo $item->sehirAdi; ?> / <em><?php echo $item->ulkeAdi; ?></em></a></h5>
+                    <h5 class="card-title pt-2"><a href="pages.php?id=<?php echo $item->id; ?>" style="text-decoration: none;" class="text-dark"><?php echo $item->sehirAdi; ?> / <em><?php echo $item->ulkeAdi; ?></em></a></h5>
                     <p class="card-text me-2"><?php echo $item->sehirAciklama; ?></p>
                 </div>
             </div>
