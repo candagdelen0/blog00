02a122f1529cd0092e4b44cf110f2fa1ba1011ad
candagdelen0 <candagdelen0@gmail.com>
2023-08-08 20:32:31 +0300

    Resimler

--- partial/_about.php
+++ partial/_about.php
@@ -6,7 +6,7 @@
     foreach ($about->getAbout() as $item):
         ?><div class="card mt-3">
         <div class="row text-center">
-            <div class="col-md-12 mt-2"><img src="<?php echo $item->picture; ?>" class="rounded-circle"></div>
+            <div class="col-md-12 mt-2"><img src="<?php echo $item->picture; ?>" class="rounded-circle" style="width: 200px; height: 250px;"></div>
             <div class="col-md-12">
                 <h5 class="card-title mt-2"><?php echo $item->title; ?></h5>
                 <p class="card-text me-2 ms-2"><?php echo $item->aboutText; ?></p>
