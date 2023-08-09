c78b6ff0998d3ddc6c9b5dcf2e244be1352dbae8
candagdelen0 <candagdelen0@gmail.com>
2023-08-09 17:37:30 +0300

    Kullanıcı Sayfası | Sağ taraf

--- userpage.php
+++ userpage.php
@@ -14,8 +14,20 @@
 <div class="container">
     <div class="row">
         <div class="col-md-4">
-        
-        </div>
+            <?php foreach ($sistem->genelsorgu($sql) as $user) {
+                echo '<div class="card mt-3 bg-light">
+                    <div class="row text-center">
+                        <div class="col-md-12 mt-2"><img src="'.$user->resim.'" class="rounded-circle" style="width: 200px; height: 250px;"></div>
+                        <div class="col-md-12">
+                            <h5 class="card-title mt-2">'.$username.'</h5>
+                            <p class="card-text me-2 ms-2">
+                                <a href="create-text.php?userid='.$user->id.'" style="text-decoration: none;" class="btn btn-primary text-white mb-4 mt-2 me-2">Yeni Yazı Ekle</a>
+                            </p>
+                        </div>
+                    </div>
+                </div>';
+            }
+        ?></div>
         <div class="col-md-8">
             <table class="table table-bordered table-striped mt-3">
                 <thead>
