a50ae76174437cb3369a8f27e6aed0235fb60d80
candagdelen0 <candagdelen0@gmail.com>
2023-08-11 13:45:24 +0300

    Seyahat Yazısı Düzenleme | Ana form

---
+++ partial/_edit-city.php
@@ -0,0 +1,39 @@
+<?php
+ 
+
+foreach ($sistem->genelsorgu($sql) as $item):
+    ?><div class="container my-3">
+        <div class="row">
+            <form method="post">
+                <div class="col-md-10 mx-auto">
+                    <div class="bg-primary mt-3 p-3 fs-3 text-center">Seyahat Yazısı Düzenleme</div>
+                    <div class="p-4" style="background-color: #F0F8FF;">
+                        <div class="mb-3">
+                            <label for="country">Ülke Adı</label>
+                            <input type="text" name="country" class="form-control" value="<?php echo $item->ulkeAdi; ?>" required>
+                        </div>
+                        <div class="mb-3">
+                            <label for="city">Şehir Adı</label>
+                            <input type="text" name="city" class="form-control" value="<?php echo $item->sehirAdi; ?>" required>
+                        </div>
+                        <div class="mb-3">
+                            <label for="description">Tanıtım Yazısı</label>
+                            <textarea name="description" class="form-control" required><?php echo $item->sehirAciklama; ?></textarea>
+                        </div>
+                        <div class="mb-3">
+                            <label for="metin">Metin</label>
+                            <textarea name="metin" class="form-control" required><?php echo $item->metin; ?></textarea>
+                        </div>
+                        <div class="mb-2">
+                            <label for="picture">Resim Yükleme Alanı</label>
+                            <input type="text" name="picture" class="form-control" value="<?php echo $item->sehirResmi; ?>">
+                        </div>
+                        <div class="mb-5">
+                            <button type="submit" name="submit" class="btn btn-primary float-end" style="width: 20%;">Kaydet</button>
+                        </div>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+<?php endforeach; 
