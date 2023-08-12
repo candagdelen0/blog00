59a516f9608cbf1b7936cbecd4d0d4fe596786ae
candagdelen0 <candagdelen0@gmail.com>
2023-08-12 19:34:37 +0300

    Yazı Ekleme | Resim dosya entegresi

--- create-text.php
+++ create-text.php
@@ -8,24 +8,52 @@
     $sistem = new Blog();
     $userid = $_GET["userid"];
 
-    if(isset($_POST["submit"])) {
+
+    if (isset($_POST["btnFileUpload"]) && $_POST["btnFileUpload"]=="Yükle") {
         $country = $sistem->safety($_POST["country"]);
         $city = $sistem->safety($_POST["city"]);
         $description = $sistem->safety($_POST["description"]);
         $metin = $sistem->safety($_POST["metin"]);
-        $picture = $sistem->safety($_POST["picture"]);
+   
+        if(isset($_FILES["fileToUpload"]) && $_FILES["fileToUpload"]["error"] == 0) {
+            $uploadOk = true;
+            $dest_path = "resimler/gezi/";
+            $filename = $_FILES["fileToUpload"]["name"];
+            $dosya_uzantilari = array('jpg','png','jpeg');
 
-        $sql = "INSERT INTO sehirler SET userid=?, ulkeAdi=?, sehirAdi=?, sehirResmi=?, sehirAciklama=?, metin=?";
-        $query = $sistem->sorgu($sql);
-        $query->execute([$userid, $country, $city, $picture, $description, $metin]);
-        echo '<div class="alert alert-success">Kayıt Başarıyla Tamamlandı</div>';
-        header('refresh:2, url=userpage.php');
+            $dosyaAdi = explode(".", $filename); 
+            $dosyaAdi_uzantisiz = $dosyaAdi[0];
+            $dosyaAdi_uzantisi = $dosyaAdi[1];
+
+            if(!in_array($dosyaAdi_uzantisi, $dosya_uzantilari)) {
+                $uploadOk = false;
+                echo '<div class="alert alert-danger">dosya uzantısı kabul edilmiyor</div>';
+                echo '<div class="alert alert-danger">kabul edilen dosyalar: </div>'.implode(",", $dosya_uzantilari);
+                echo "<br>";
+            } 
+
+            $yeni_dosyaAdi = md5(time().$dosyaAdi_uzantisiz).'.'.$dosyaAdi_uzantisi;
+            $fileSourcePath = $_FILES["fileToUpload"]["tmp_name"];
+            $fileDestPath = $dest_path.$yeni_dosyaAdi;
+
+            if($uploadOk) {
+                if(move_uploaded_file($fileSourcePath, $fileDestPath)) {
+                    $sql = "INSERT INTO sehirler SET userid=?, ulkeAdi=?, sehirAdi=?, sehirResmi=?, sehirAciklama=?, metin=?";
+                    $query = $sistem->sorgu($sql);
+                    $query->execute([$userid, $country, $city, $fileDestPath, $description, $metin]);
+                    echo '<div class="alert alert-success">Kayıt Başarıyla Tamamlandı</div>';
+                    header('refresh:2, url=userpage.php');
+                } 
+            }
+        }
     }
+
+
 ?>
 
 <div class="container my-3">
     <div class="row">
-        <form method="post">
+        <form method="post" enctype="multipart/form-data">
             <div class="col-md-8 mx-auto">
                 <div class="bg-primary mt-3 p-3 fs-3 text-center">Yeni Seyahat Yazısı</div>
                 <div class="p-4" style="background-color: #F0F8FF;">
@@ -45,11 +73,11 @@
                         <label for="metin">Metin</label>
                         <textarea name="metin" class="form-control" required></textarea>
                     </div>
-                    <div class="mb-3">
-                        <label for="picture">Resim Yükleme Alanı</label>
-                        <input type="text" name="picture" class="form-control">
+                    <div class="mb-2">
+                        <label for="fileToUpload">Resim Yükleme Alanı</label>
+                        <input type="file" name="fileToUpload" class="form-control">
+                        <input type="submit" class="btn btn-primary mt-3 form-control" value="Yükle" name="btnFileUpload">
                     </div>
-                    <button type="submit" name="submit" class="btn btn-primary">Kaydet</button>
                 </div>
             </div>
         </form>
