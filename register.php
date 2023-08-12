8a203f48bd877627e800ef9f0aaa45df0aec1955
candagdelen0 <candagdelen0@gmail.com>
2023-08-12 19:42:44 +0300

    Kullanıcı Kayıt | Resim Yükleme Entegresi

--- register.php
+++ register.php
@@ -21,19 +21,46 @@ if ($_SERVER["REQUEST_METHOD"]=="POST") {
         $password = $sistem->safety($_POST["password"]);
     }
 
-    if(empty($_POST["password"])) {
+    if(empty($_POST["repassword"])) {
         $repasswordErr = "Parola tekrarı boş bırakılamaz";
     } else {
         $repassword = $sistem->safety($_POST["repassword"]);
     }
 
-    if ($username != "" && $password != "" && $repassword != "") {
-        $sql = "INSERT INTO users SET username = ?, parola = ?";
-        $query = $sistem->SignUp($sql);
-        $query->execute([$username, $password]);
-        echo '<div class="col-md-4 mx-auto mt-2 alert alert-success">Kayıt Başarılı</div>';
-        header("refresh: 2, url=login.php");
-    } 
+    if(isset($_FILES["fileToUpload"]) && $_FILES["fileToUpload"]["error"] == 0) {
+        $uploadOk = true;
+        $dest_path = "resimler/user/";
+        $filename = $_FILES["fileToUpload"]["name"];
+        $dosya_uzantilari = array('jpg','png','jpeg');
+
+        $dosyaAdi = explode(".", $filename); 
+        $dosyaAdi_uzantisiz = $dosyaAdi[0];
+        $dosyaAdi_uzantisi = $dosyaAdi[1];
+    
+        if(!in_array($dosyaAdi_uzantisi, $dosya_uzantilari)) {
+            $uploadOk = false;
+            echo '<div class="alert alert-danger">dosya uzantısı kabul edilmiyor</div>';
+            echo '<div class="alert alert-danger">kabul edilen dosyalar: </div>'.implode(",", $dosya_uzantilari);
+            echo "<br>";
+        } 
+    
+        $yeni_dosyaAdi = md5(time().$dosyaAdi_uzantisiz).'.'.$dosyaAdi_uzantisi;
+        $fileSourcePath = $_FILES["fileToUpload"]["tmp_name"];
+        $fileDestPath = $dest_path.$yeni_dosyaAdi;
+    
+        if($uploadOk) {
+            if(move_uploaded_file($fileSourcePath, $fileDestPath)) {
+                if($username != "" && $password != "" && $repassword != "") {
+                    $sql = "INSERT INTO users SET username = ?, parola = ?, resim = ?";
+                    $query = $sistem->sorgu($sql);
+                    $query->execute([$username, $password, $fileDestPath]);
+                    echo '<div class="col-md-4 mx-auto mt-2 alert alert-success">Kayıt Başarılı</div>';
+                    header("refresh: 2, url=login.php");
+                }
+            } 
+        }
+    }
+    
 }
 
 ?>
@@ -42,7 +69,7 @@ if ($_SERVER["REQUEST_METHOD"]=="POST") {
         <div class="col-md-4 mx-auto">
             <div class="bg-primary mt-3 p-3 fs-3 text-center">Üyelik İşlemleri</div>
             <div class="p-4" style="background-color: #F0F8FF;">
-                <form action="register.php" method="post">
+                <form action="register.php" method="post" enctype="multipart/form-data">
                     <div class="mb-3">
                         <label for="username">Kullanıcı Adı</label>
                         <input type="text" name="username" class="form-control">
@@ -58,7 +85,11 @@ if ($_SERVER["REQUEST_METHOD"]=="POST") {
                         <input type="password" name="repassword" class="form-control">
                         <div class="text-danger"><?php echo $repasswordErr; ?></div>
                     </div>
-                    <button type="submit" class="btn btn-primary" name="register">Üye Ol</button>
+                    <div class="mb-2">
+                        <label for="fileToUpload">Resim Yükleme Alanı</label>
+                        <input type="file" name="fileToUpload" class="form-control">
+                        <input type="submit" class="btn btn-primary mt-3 form-control" value="Yükle" name="btnFileUpload">
+                    </div>
                 </form>
             </div>
         </div>
