cedd16f0fabf4174b5676d49bbb2cfec834e1054
candagdelen0 <candagdelen0@gmail.com>
2023-08-18 15:15:14 +0300

    Reklam Alanı Güncellemesi

--- partial/_about.php
+++ partial/_about.php
@@ -2,15 +2,15 @@
     require_once "functions/connection.php"; 
     require_once "functions/function.php"; 
     
-    $about = new Blog();
-    $sql = "SELECT * FROM about";
-    foreach ($about->genelsorgu($sql) as $item):
+    $reklam = new Blog();
+    $sql = "SELECT * FROM reklam";
+    foreach ($reklam->genelsorgu($sql) as $item):
         ?><div class="card mt-3">
         <div class="row text-center">
-            <div class="col-md-12 mt-2"><img src="<?php echo $item->picture; ?>" class="rounded-circle" style="width: 200px; height: 250px;"></div>
+            <div class="col-md-12 mt-2"><img src="<?php echo $item->picture; ?>" class="rounded" style="width: 200px; height: 250px;"></div>
             <div class="col-md-12">
                 <h5 class="card-title mt-2"><?php echo $item->title; ?></h5>
-                <p class="card-text me-2 ms-2"><?php echo $item->aboutText; ?></p>
+                <p class="card-text me-2 ms-2"><?php echo $item->aciklama; ?></p>
             </div>
         </div>
     </div>
